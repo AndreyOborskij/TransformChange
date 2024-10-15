30e009d9abf649bd452a1a706e5a29c10cb1c047
Андрей <120372809+AndreyOborskij@users.noreply.github.com>
2024-10-15 14:05:01 +0300

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,7 +4,7 @@ public class Mover : MonoBehaviour
 {
      [SerializeField] private Vector3 _speedDirection;
 
-     void Update()
+     private void Update()
      {
          transform.position += _speedDirection;
      }
